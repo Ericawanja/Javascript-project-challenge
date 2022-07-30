d2fdd01006ad10112a679df9113c9fdbb17ea095
Ericawanja <wanjaerica@gmail.com>
2022-07-30 22:03:13 +0300

    Dom and web storage api

--- 1. Movie seat booking/scripts/main.js
+++ 1. Movie seat booking/scripts/main.js
@@ -1,7 +1,9 @@
-const audince= document.getElementsByClassName("audience")[0]
+const container = document.querySelector('.container')
 const row1 = document.getElementsByClassName("row1")[0]
 const row2 = document.getElementsByClassName("row2")[0]
 const row3 = document.getElementsByClassName("row3")[0]
+
+const selectBtn = document.getElementById('movies')
 
 const row1Seats= [1,2,3,4,5,6,7,8,9,10]
 const row2Seats = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
@@ -26,14 +28,43 @@ row3.innerHTML = row3Seats.map((seat)=>{
 
 
 /*************************interactivity */
-const seats= document.getElementsByClassName('seat')
-console.log(seats)
-for(let i=0; i<seats.length;i++){
-seats[i].addEventListener("click", seatsFunction )
 
+console.log(container)
+container.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('seat')&& !e.target.classList.contains('occupied')){
+       // e.target.classList.toggle('selected')
+        updateSelectedCount()
+    }
+})
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.seat.selected')
+    localStorage.setItem("seatCount", selectedSeats.length)   
+    let costStored= localStorage.getItem('cost') !== null ?localStorage.getItem('cost') : 0
+    console.log(selectedSeats.length)
+    
+     document.getElementById('seatCount').innerHTML= `<h3>seats selected: ${selectedSeats.length}</h3>`
+     document.getElementById('totalCost').innerHTML=`<h2>Total cost: ${selectedSeats.length * costStored}</h2>`  
 }
+//onChange of the movie
 
-function seatsFunction(e){
-e.classList.contains
+selectBtn.addEventListener('change', (e)=>{
+    let movie=e.target.value
+    document.getElementById('movieSelected').innerHTML = `<h3>Movie selected is: ${movie.toUpperCase()}</h3>`
+    
+    let cost=0;
+    
+    if( movie == 'avengers'){
+        cost= 10
+    }
+    if( movie == 'uglyDolls'){
+        cost=15
+    }
+    if(movie == 'lionKing'){
+        cost = 20
+    }
+    console.log(cost)
+    localStorage.setItem('cost', cost) 
+    updateSelectedCount()
 
-}
+})
+updateSelectedCount()
