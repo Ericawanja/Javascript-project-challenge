f73602b9598239e98f59367108f3cd87bd1ab4d3
Ericawanja <wanjaerica@gmail.com>
2022-08-03 19:18:59 +0300

    Quote Documentation

---
+++ 4- Random Quote generator/README.md
@@ -0,0 +1,18 @@
+# Random Quote Generator
+This app provides three main fuctionalities.
+
+1. Generate the quote by clicking New Quote button. 
+2. Convert the text to speech using web speech API
+3. Directly tweet the quote
+4. Copy the quote 
+
+## API's used
+1. Quote generating API - http://api.quotable.io
+2. Twitter API to tweet the quote
+3. Window API to copy the quote 
+4. Window Speech API to convert the text to speech
+
+## Concepts learnt
+
+- API's using vanilla Javascript
+- Dom manipulation 
--- 4- Random Quote generator/scripts/main.js
+++ 4- Random Quote generator/scripts/main.js
@@ -1,48 +1,47 @@
-const btn = document.getElementsByClassName('btn')[0]
-const quote = document.getElementsByClassName('quote')[0]
-const author = document.getElementsByClassName('author')[0]
-const copy = document.getElementsByClassName('copy')[0]
-const msg = document.getElementsByClassName('speech')[0]
-const tweetBtn =document.getElementsByClassName('twitter')[0]
+const btn = document.getElementsByClassName("btn")[0];
+const quote = document.getElementsByClassName("quote")[0];
+const author = document.getElementsByClassName("author")[0];
+const copy = document.getElementsByClassName("copy")[0];
+const msg = document.getElementsByClassName("speech")[0];
+const tweetBtn = document.getElementsByClassName("twitter")[0];
 
-btn.addEventListener('click', getQuote)
-function getQuote(){
-    btn.classList.add("loading");
-    btn.innerText = "Loading Quote...";
-    fetch("http://api.quotable.io/random")
-    .then(response => response.json())
-    .then(data => {
-        quote.innerHTML =`${data.content}`
-        author.innerHTML =`${data.author}`
-        btn.classList.remove("loading");
-        btn.innerHTML = `<button>New Quote</button>`
-    })
-
+btn.addEventListener("click", getQuote);
+function getQuote() {
+  btn.classList.add("loading");
+  btn.innerText = "Loading Quote...";
+  fetch("http://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      quote.innerHTML = `${data.content}`;
+      author.innerHTML = `${data.author}`;
+      btn.classList.remove("loading");
+      btn.innerHTML = `<button>New Quote</button>`;
+    });
 }
 //copy
-copy.addEventListener('click', copyText)
+copy.addEventListener("click", copyText);
 
-function copyText(){
-    return navigator.clipboard.writeText(quote.innerText);
+function copyText() {
+  return navigator.clipboard.writeText(quote.innerText);
 }
 
 //speech
-msg.addEventListener('click', convertToSpeech)
+msg.addEventListener("click", convertToSpeech);
 
-function convertToSpeech(){
-    const words = document.getElementsByClassName('quote')[0].innerHTML
-    console.log(words)
-    const speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US"
-    speech.text = words
-    speech.volume = 1
-    speech.rate = 1
-    speech.pitch = 1
+function convertToSpeech() {
+  const words = document.getElementsByClassName("quote")[0].innerHTML;
+  console.log(words);
+  const speech = new SpeechSynthesisUtterance();
+  speech.lang = "en-US";
+  speech.text = words;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
 
-    window.speechSynthesis.speak(speech)
+  window.speechSynthesis.speak(speech);
 }
 //tweet
-tweetBtn.addEventListener("click", ()=>{
-    let tweetUrl = `https://twitter.com/intent/tweet?url=${quote.innerText}`;
-    window.open(tweetUrl, "_blank");
+tweetBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quote.innerText}`;
+  window.open(tweetUrl, "_blank");
 });
