e97f3ebd48b2f42cdbd0aa74e9de9237e9b11b7a
Ericawanja <wanjaerica@gmail.com>
2022-08-09 23:42:11 +0300

    interactivity

--- 7. Carousel/index.js
+++ 7. Carousel/index.js
@@ -1,21 +1,53 @@
-  const images=[
-    {src:'./images/image0.jpg'},
-    {src:'./images/image1.jpg'},
-    {src:'./images/image2.jpg'},
-    {src:'./images/image3.jpg'},
-    {src:'./images/image4.jpg'},
-    {src:'./images/image5.jpg'},
-    {src:'./images/image6.jpg'},
-    {src:'./images/image4.jpg'},
+const images = [
+  {
+    src: "./images/image0.jpg",
+    caption: "one",
+  },
+  { src: "./images/image1.jpg", caption: "one" },
+  { src: "./images/image2.jpg", caption: "one" },
+  { src: "./images/image3.jpg", caption: "one" },
+  { src: "./images/image4.jpg", caption: "one" },
+  { src: "./images/image5.jpg", caption: "one" },
+  { src: "./images/image6.jpg", caption: "one" },
+  { src: "./images/image4.jpg", caption: "one" },
+];
+const imgContainer = document.getElementsByClassName("carousel")[0];
+imgContainer.innerHTML = images
+  .map((img) => {
+    return `
+    <div class="carousel-item">  
+        <div class='index'>1/4</div>
+        <img src='${img.src}' alt=""/>
+        <div class="text">Caption ${img.caption}</div>
+    </div>
+    `;
+  })
+  .join("");
 
-  ]
-  const imgContainer = document.getElementsByClassName("carousel")[0]
-  imgContainer.innerHTML= images.map(img=>{
-    return`
-    <div class="carousel-item">
-            <img src='${img.src}' alt="">
-        </div>
-    `
-  }).join("")
+document.getElementsByClassName("carousel-item")[5]
+.classList.add("carousel-item-visible");
 
-  document.querySelector('.carousel-item').classList.add("carousel-item-visible")
+let slideIndex = 0
+function plusSlides(n){
+  showSlides(slideIndex += n)
+
+}
+function showSlides(n){
+
+  let slides = document.getElementsByClassName('carousel-item');
+  if( n > slides.length) slideIndex = 1;
+  if(n < 1) slideIndex = slides.length;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  for (let i=0; i<slides.length; i++){
+    if(i == slideIndex){
+      console.log(i, slideIndex)
+      slides[i].classList.replace("carousel-item","carousel-item-visible")
+   }
+  }
+  slides[slideIndex-1].style.display = "block";
+  
+}
