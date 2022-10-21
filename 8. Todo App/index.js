c2360f40d0c941d7269d38bdbc2db22831561286
Ericawanja <wanjaerica@gmail.com>
2022-10-21 20:59:24 +0300

    mapped data

--- 8. Todo App/index.js
+++ 8. Todo App/index.js
@@ -1,34 +1,70 @@
 const taskContainer = document.querySelector(".task_container");
 
-const fetchTodos = async () => {
-  const result = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const todos = await result.json();
 
-  return todos;
-};
+class MyApp {
+  todos = [];
 
-// (async ()=>{
-//     console.log(await fetchTodos());
-// })()
+  async initializeApp() {
+    await this.fetchTodos();    
 
-// fetchTodos()
-//   .then(console.log)
-//   .catch((err) => {
-//     console.log({ err });
-//   });
-
-const main = async () => {
-  try {
-    let myTodos = await fetchTodos();
-    console.log("MY TODOS", myTodos);
-    // Show them to UI
-  } catch (error) {
-    console.log({error});
   }
 
-  
-};
+  fetchTodos = async () => {
+    const result = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const todos = await result.json();
+    this.todos = todos;
 
-main()
+    this.renderTodos()
+  };
+
+  addTodo = async (data) => {
+    const result = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const todo = await result.json();
+
+    this.todos.unshift(todo);
+  };
 
 
+  renderTodos(){
+    const todosMarkup = this.todos.map(todo=>`
+    <div class="task">
+    <span class="input"
+      ><input
+        type="checkbox"
+        name="completed_status"
+        id=${todo.id}
+        class="checkbox"
+    /></span>
+    <span class="title" id=${todo.id}>${todo.title}</span>
+    <span class="delete">
+      <svg
+        data-id="${todo.id}"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style="fill: rgba(0, 0, 0, 0.5); transform: ; msfilter: "
+      >
+        <path
+          d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm4 12H8v-9h2v9zm6 0h-2v-9h2v9zm.618-15L15 2H9L7.382 4H3v2h18V4z"
+        ></path>
+      </svg>
+    </span>
+  </div>
+    
+    `).join('')
+
+    taskContainer.innerHTML = todosMarkup;
+    
+  }
+}
+
+const app = new MyApp();
+app.initializeApp();
